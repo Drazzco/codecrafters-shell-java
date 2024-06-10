9c172b6f5d0d9e6bec197f0936f806069426aed8
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-06-10 16:39:14 -0300

    Echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,32 @@ public class Main {
         while(true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            switch(input) {
-                case "exit 0":
-                    System.exit(0);
+            String[] str = input.split(" ");
+            String command = str[0];
+            String parameter = "";
+
+            if(str.length > 2) {
+                for(int i = 1; i < str.length; i++) {
+                    if(i < str.length-1) {
+                        parameter += str[i] + " ";
+                    } else {
+                        parameter += str[i];
+                    }
+                }
+            } else {
+                parameter = str[1];
+            }
+
+            switch(command) {
+                case "exit":
+                    if(parameter.equals("0")) {
+                        System.exit(0);
+                    } else {
+                        System.out.println(input + ": command not found");
+                    }
+                    break;
+                case "echo":
+                    System.out.println(parameter);
                     break;
                 default:
                 System.out.println(input + ": command not found");
