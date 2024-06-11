1fc785e6f2c44208b9d8499a951b3dc488ff61dc
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-06-10 21:40:58 -0300

    type builtin: builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class MainShell { // 1
+public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         List<String> builtins = builtins();
